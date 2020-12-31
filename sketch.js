d48357a5d35e6ef6b95f51efcfbef9a6f65f1c44
Tejbeer23 <tejbeerkaur1234@gmail.com>
2020-12-31 19:48:19 +0530

    add a box

--- sketch.js
+++ sketch.js
@@ -21,6 +21,7 @@ function setup(){
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
+    box3 = new Box(300,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
 
@@ -46,6 +47,7 @@ function draw(){
     console.log(box2.body.angle);
     box1.display();
     box2.display();
+    box3.display();
     ground.display();
     pig1.display();
     log1.display();
